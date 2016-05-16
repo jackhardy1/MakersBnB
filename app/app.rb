e746edff7955ba7199a38a918f3344693577f319
Elia Bardashevich <ebardashevich@gmail.com>
2016-05-16 16:30:48 +0100

    adds the test for the space

--- app/app.rb
+++ app/app.rb
@@ -1,9 +1,14 @@
 require 'sinatra/base'
+require_relative 'data_mapper_setup'
+
 
 class Bnb < Sinatra::Base
   get '/' do
     'Hello Bnb!'
   end
+
+
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -4,7 +4,8 @@ ENV['RACK_ENV'] ||= 'development'
 require 'data_mapper'
 require 'dm-postgres-adapter'
 require_relative 'models/user'
-require_relative 'model/space'
+require_relative 'models/space'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/makersbnb_#{ENV['RACK_ENV']}")
 DataMapper.finalize
+DataMapper.auto_migrate!
