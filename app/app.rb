159b8c5ebf9c8ac4e223b2100f98ebc4571265c7
Macey Baker <maceybaker@Maceys-MacBook-Pro-8.local>
2016-05-17 10:26:30 +0100

    Trying to prevent merge issues

--- app/app.rb
+++ app/app.rb
@@ -1,34 +1,22 @@
+ENV['RACK_ENV'] ||= 'development'
+
 require 'sinatra/base'
 require 'sinatra/flash'
 require_relative 'data_mapper_setup'
-require_relative 'models/user'
+require_relative 'controllers/sign-up'
+require_relative 'controllers/spaces'
+require_relative 'server'
 
 class Bnb < Sinatra::Base
 
-  enable :sessions
-  set :session_secret, 'super secret'
-  register Sinatra::Flash
+  set :root, File.join(File.dirname(__FILE__), '')
 
   get '/' do
     'Hello Bnb!'
   end
 
-  get '/sign-up' do
-    erb :signup
-  end
 
-  post '/users' do
-    user = User.create(firstname: params[:firstname], lastname: params[:lastname], email: params[:email], password: params[:password])
-    if user.save
-      redirect '/'
-    elsif User.first(email: params[:email])
-      flash[:notice] = "Must have unique email"
-      redirect '/sign-up'
-    else
-      flash[:notice] = "Email is not valid"
-      redirect '/sign-up'      
-    end
-  end
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
