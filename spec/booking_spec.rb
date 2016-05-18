955706a289affe66cce500351f7023a81e234b2c
Matthew Ward <iammatthewward@gmail.com>
2016-05-18 10:38:26 +0100

    Fixed failing tests for booking

--- spec/booking_spec.rb
+++ spec/booking_spec.rb
@@ -6,19 +6,21 @@ describe Booking do
 
     before(:each) do
       user = User.create(firstname: 'Jack', lastname: 'Hardy', email: 'jackhardy1@gmail.com', password: 'jack')
-      space = Space.create(name: 'makers', description: 'makers is good', price: 10)
+      space = Space.create(name: 'makers', description: 'makers is good', price: 10, user_id: user.id)
       booking = Booking.create
-      booking.space << space
-      booking.user << user
+      space.bookings << booking
+      user.bookings << booking
       booking.save
     end
 
     it 'booking has a user' do
-      expect(Booking.first.user.name).to eq 'Jack'
+      our_booking = Booking.first
+      expect(our_booking.user.id).to eq 1
     end
 
     it 'booking has a space' do
-      expect(Booking.space.name).to eq 'makers'
+      our_booking = Booking.first
+      expect(our_booking.space.name).to eq 'makers'
     end
   end
 end
